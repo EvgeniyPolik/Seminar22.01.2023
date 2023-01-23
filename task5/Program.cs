304066572390049716186f42df2b8e8bfe2bbdcb
poliex <poliex86@gmail.com>
2023-01-23 23:06:43 +0500

    Task5 complete

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N: ");
+var n = int.Parse(Console.ReadLine() ?? "");
+var prod = 1;
+if (n == 0)
+    prod = 0;
+else
+    for (int i = 1; i <= n; i++)
+        prod *= i;
+    Console.WriteLine($"Произведение всех чисел до числа {n} равна: {prod}");   
