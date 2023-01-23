9bfe77d39e3fec3346664cea2f517be48c1eedcf
poliex <poliex86@gmail.com>
2023-01-23 20:40:42 +0500

    Task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число N: ");
+var n = int.Parse(Console.ReadLine() ?? "");
+Console.Write($"Все кубы чисел до {n}: ");
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(Math.Pow(i, 3));
+    Console.Write(" ");
+}
