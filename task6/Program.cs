1359e67e750fba161a95316b38ac29a38825de49
poliex <poliex86@gmail.com>
2023-01-24 20:42:06 +0500

    Task6 complete

---
+++ task6/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите N: ");
+var n = int.Parse(Console.ReadLine() ?? "");
+for (int i = 0; i <= n; i++)
+{
+    int num = (int)Math.Pow(i, 3);
+    if (num % 2 == 0)
+        Console.WriteLine(num); 
+}
+
+/* ver2
+IEnumerable<int> GenerateSequence() 
+{
+    var i = 0;
+    while (true)
+    {
+        int num = (int)Math.Pow(i, 3);
+        if (num % 2 == 0)
+            yield return num; 
+        i++;      
+    }
+}
+
+foreach (var item in GenerateSequence())
+{
+    Console.WriteLine(item);
+    Thread.Sleep(500);
+    if (Console.KeyAvailable) break;
+}
+*/
