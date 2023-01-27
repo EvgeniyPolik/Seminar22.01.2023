4ae18ef87557ff65349ddee69810696718273edc
poliex <poliex86@gmail.com>
2023-01-27 20:22:16 +0500

    Task7 complete

---
+++ task7/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] binaryArray = new int[8];
+var rnd = new Random();
+for (int i = 0; i < 8; i++)
+    binaryArray[i] = rnd.Next(2);
+Console.Write("Сформированный массив: ");
+var sep = " ";
+foreach (var element in binaryArray)
+{
+    Console.Write(element);
+    Console.Write(sep);
+}
